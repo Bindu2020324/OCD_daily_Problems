a3298af9edc2d8e96fd6af2c9e7739c02ef6b95a
Bindu2020324 <bindurunwal054@gmail.com>
2025-02-27 16:11:05 +0530

    Add files via upload

---
+++ uglyno.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Solution {
+    public:
+        bool isUgly(int n) {
+            // for(int i=2;i<n;i++){
+                // m1->wrong
+                // if(n==5) continue;
+                // else if(n%i==0){
+                //     return false;
+                // }
+                // m2->wrong
+                // if(i%2!=0 || i%3!=0 || i%5!=0){
+                //     if(n%i==0){
+                //         return false;
+                //     }
+                // }
+            // }
+            // return true;
+    
+                if(n<=0) return false;
+                else{
+                    while(n%2==0) n/=2;
+                    while(n%3==0) n/=3;
+                    while(n%5==0) n/=5;
+                }
+                return n==1;
+            
+        }
+    };
+int main(){
+    int n=8;
+    if(isUgly(n)) cout<<("yes");
+    else cout<<("no");
+    return 0;
+}
