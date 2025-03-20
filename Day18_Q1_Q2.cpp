e8fec98e973238a038f3ff1184e4994a3ff2f82a
Bindu2020324 <bindurunwal054@gmail.com>
2025-03-20 19:58:37 +0530

    anagram

---
+++ Day18_Q1_Q2.cpp
@@ -0,0 +1,25 @@
+Q1.  leetcode  242 
+  tc=0(n)
+// anagram->An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, using all the original letters exactly once.
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //TC=O(n)
+        vector<int> srr(256,0);
+        for(int i=0;i<s.size();i++){
+            int idx=int(s[i]);
+            srr[idx]++;
+        }
+        vector<int> trr(256,0);
+        for(int i=0;i<t.size();i++){
+            int idx=int(t[i]);
+            trr[idx]++;
+        }
+
+        if(srr==trr) return true;
+        else return false;
+
+
+    }
+};
